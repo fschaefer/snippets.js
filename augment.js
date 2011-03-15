6e19cf0b2fe9c3e1238a59bea4b6f0aa6ef4e2aa
Florian Schäfer <florian.schaefer@gmail.com>
2011-03-15 13:20:37 +0100

    Initial commit.

---
+++ augment.js
@@ -0,0 +1,19 @@
+(function(exports){
+    
+    function augment (receiving, giving) {
+        if (arguments[2]) {
+            for (var i = 2, len = arguments.length; i < len; i++) {
+                receiving[arguments[i]] = giving[arguments[i]];
+            }
+        } else {
+            for (var method in giving) {
+                if (!receiving[method]) {
+                    receiving[method] = giving[method];
+                }
+            }
+        }
+    };
+    
+    exports.augment = augment;
+    
+})(this || {});
